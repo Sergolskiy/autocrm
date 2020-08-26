761cb51920b99706195916570f5a0d9d48215374
ARTEM <girukartem@gmail.com>
2020-08-26 16:42:36 +0300

    reacord

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -7,6 +7,17 @@ import CreateTaskPopup from "../../components/ModalPopups/CreateTaskPopup/Create
 import Time from "../../components/Time/Time";
 
 
+import TableBody from "@material-ui/core/TableBody";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import Table from "@material-ui/core/Table";
+import TableContainer from "@material-ui/core/TableContainer";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import '../Recording/Recording.css'
+
+
 class Dashboard extends Component {
 
   state = {
@@ -190,6 +201,98 @@ class Dashboard extends Component {
                 </div>
               </div>
 
+            </div>
+          </div>
+        </div>
+
+
+        <div className="dashboard__actual-task">
+          <span className={'header__name'}>
+              <FormattedMessage id="app.recording"/>
+            </span>
+        </div>
+        <div>
+          <div className="recording-head">
+            <div className="recording__filter">
+              <span><FormattedMessage id="app.selectDate"/></span>
+              <TextField
+                type="date"
+                defaultValue=""
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+            </div>
+          </div>
+          <div className="page-section">
+            <div className="page-section__name">
+              <FormattedMessage id="app.today"/>
+            </div>
+            <div className="page-section__content">
+              <TableContainer className={'recording-table'}>
+                <Table size="small" aria-label="a dense table">
+                  <TableHead>
+                    <TableRow className={'clients-table-head'}>
+                      <TableCell><FormattedMessage id="app.time"/></TableCell>
+                      <TableCell align="left"><FormattedMessage id="app.task"/></TableCell>
+                      <TableCell align="left"><FormattedMessage id="app.clientName"/></TableCell>
+                      <TableCell align="left"><FormattedMessage id="app.clientPhone"/></TableCell>
+                      <TableCell align="left"><FormattedMessage id="app.responsible"/></TableCell>
+                      <TableCell align="right"></TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    <TableRow className={'clients-table-row'}>
+                      <TableCell align="left">9:00</TableCell>
+                      <TableCell align="left">Название задачи</TableCell>
+                      <TableCell align="left">Олег Побережный</TableCell>
+                      <TableCell align="left">+380999999999</TableCell>
+                      <TableCell align="left">Андрей Виллин</TableCell>
+                      <TableCell align="right">
+                        <Button variant="contained" color="primary" size="small">
+                          <FormattedMessage id="app.edit"/>
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                    <TableRow className={'clients-table-row'}>
+                      <TableCell align="left">11:00</TableCell>
+                      <TableCell align="left">Название задачи</TableCell>
+                      <TableCell align="left">Олег Побережный</TableCell>
+                      <TableCell align="left">+380999999999</TableCell>
+                      <TableCell align="left">Андрей Виллин</TableCell>
+                      <TableCell align="right">
+                        <Button variant="contained" color="primary" size="small">
+                          <FormattedMessage id="app.edit"/>
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                    <TableRow className={'clients-table-row'}>
+                      <TableCell align="left">12:30</TableCell>
+                      <TableCell align="left">Название задачи</TableCell>
+                      <TableCell align="left">Олег Побережный</TableCell>
+                      <TableCell align="left">+380999999999</TableCell>
+                      <TableCell align="left">Андрей Виллин</TableCell>
+                      <TableCell align="right">
+                        <Button variant="contained" color="primary" size="small">
+                          <FormattedMessage id="app.edit"/>
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                    <TableRow className={'clients-table-row'}>
+                      <TableCell align="left">16:00</TableCell>
+                      <TableCell align="left">Название задачи</TableCell>
+                      <TableCell align="left">Олег Побережный</TableCell>
+                      <TableCell align="left">+380999999999</TableCell>
+                      <TableCell align="left">Андрей Виллин</TableCell>
+                      <TableCell align="right">
+                        <Button variant="contained" color="primary" size="small">
+                          <FormattedMessage id="app.edit"/>
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+              </TableContainer>
             </div>
           </div>
         </div>
